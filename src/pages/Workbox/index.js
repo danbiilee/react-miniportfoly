1abc4e57f6b2db5f465a0d960f93c51976d8daa1
danbiilee <danbi.db@gmail.com>
2020-12-14 23:32:15 +0900

    [STYLE]: 워크박스 메인페이지 수정

--- src/pages/Workbox/index.js
+++ src/pages/Workbox/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouteMatch, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Content from '../../components/Layout/Content';
 import Layout from '../../components/Layout/Layout';
 import Sidebar from '../../components/Layout/Sidebar';
@@ -8,6 +9,16 @@ import MainMenu from '../../components/Menu/MainMenu';
 import SubMenu from '../../components/Menu/SubMenu';
 import ChangeTheme from './ChangeTheme';
 import TasklistWrapper from './Tasklist';
+
+const IndexWrapper = styled.div`
+  display: flex;
+  img {
+    width: 50%;
+  }
+  p {
+    writing-mode: vertical-rl;
+  }
+`;
 
 const Workbox = () => {
   const match = useRouteMatch();
@@ -27,7 +38,18 @@ const Workbox = () => {
         <Card>
           <Switch>
             <Route exact path={match.path}>
-              <Index />
+              <IndexWrapper>
+                <img
+                  src={
+                    process.env.PUBLIC_URL + '/resources/img/workbox-main.svg'
+                  }
+                  alt="Workbox Main"
+                />
+                <p>재미난 거 만드는 법</p>
+                <p>첫 번째. 참신한 아이디어 떠올리기</p>
+                <p>두 번째. 구현할 수 있는 능력 보유하기</p>
+                <p>세 번째. 뚝딱뚝..따..ㄱ..ㅇ..뚱땅 만들기</p>
+              </IndexWrapper>
             </Route>
             <Route path={`${match.path}/changeTheme`}>
               <ChangeTheme />
@@ -41,10 +63,6 @@ const Workbox = () => {
       <MainMenu />
     </Layout>
   );
-};
-
-const Index = () => {
-  return <div>워크박스 인덱스</div>;
 };
 
 export default Workbox;
