6b00e3f98ab6dc972214272f5c1aceb5ec326bf6
danbiilee <danbi.db@gmail.com>
2020-12-14 23:33:00 +0900

    [FEAT]: 테마 변경 기능구현 끝

--- src/pages/Workbox/ChangeTheme.js
+++ src/pages/Workbox/ChangeTheme.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import styled from 'styled-components';
-import cloneDeep from 'lodash/cloneDeep';
+import styled, { css } from 'styled-components';
 import { setColor } from '../../module/palette';
 import Post from '../../components/Layout/Post';
 
@@ -28,7 +27,12 @@ const Tab = styled.li`
   width: 50px;
   height: 50px;
   border: 1px solid #a5a5a5;
-  border-bottom: 0;
+  /* border-bottom: 0; */
+  ${props =>
+    props.id === props.target &&
+    css`
+      border-bottom-color: ${props => props.color};
+    `}
   border-radius: 5px 5px 0 0;
   background: ${props => props.color};
   cursor: pointer;
@@ -39,10 +43,15 @@ const Tab = styled.li`
 `;
 
 const ChangeWrapper = styled.div`
-  margin-top: -1px;
-  border: 1px solid #a5a5a5;  
+  min-width: 710px;
+  border: 1px solid #a5a5a5;
+  border-top: 0;
   border-radius: 0 5px 5px 5px;
   background: ${props => props.color};
+  .pleasePick {
+    padding: 20px;
+    font-weight: bold;
+  }
 `;
 
 const ColorWrapper = styled.div`
@@ -56,7 +65,7 @@ const Swatch = styled.div`
   background: #fff;
   border: 1px solid #a5a5a5;
   border-radius: 3px;
-  box-shadow: 3px 3px 10px rgba(0,0,0,0.2);
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
   input {
     display: flex;
     width: 140px;
@@ -80,36 +89,34 @@ const Swatch = styled.div`
 
 const PaletteWrapper = styled.div`
   position: relative;
-  width: 60%;
   min-height: 200px;
-  height: fit-content; 
+  min-width: 500px;
+  width: 60%;
+  height: fit-content;
   padding: 20px;
   margin-left: 20px;
   background: #fff;
   border: 1px solid #a5a5a5;
   border-radius: 3px;
-`;
-
-const Arrow = styled.span`
-  position: absolute;
-  top: 85px;
-  left: -15px;
-  border-top: 15px solid transparent;
-  border-right: 15px solid #fff;
-  border-bottom: 15px solid transparent;
-  border-left: 0;
-  box-shadow: 3px 3px 10px rgba(0,0,0,0.2);
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
 `;
 
 const ColorList = styled.ul`
-  display: flex;  
-  margin-bottom: 8px;
+  display: flex;
+  &:first-of-type {
+    margin-bottom: 8px;
+  }
+  &:last-of-type {
+    margin-bottom: 15px;
+  }
 `;
 
 const Color = styled.li`
   width: 50px;
   height: 50px;
-  margin-right: 8px;
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
   background: ${props => props.color};
   border-radius: 5px;
   cursor: pointer;
@@ -117,14 +124,20 @@ const Color = styled.li`
 
 const Button = styled.button`
   height: 35px;
-  padding: 0 10px;
-  border-radius: 10px;
-  border: 1px solid;
+  margin-right: 8px;
+  padding: 0 20px;
+  border-radius: 20px;
+  border: 2px solid;
+  background: #fff;
   line-height: 35px;
   font-weight: bold;
-  font-size: .9rem;
+  font-size: 0.9rem;
   outline: 0;
   cursor: pointer;
+  &:hover {
+    background: #333;
+    color: #fff;
+  }
 `;
 
 const ChangeTheme = () => {
@@ -132,28 +145,43 @@ const ChangeTheme = () => {
   const dispatch = useDispatch();
   const { layout: palette } = useSelector(state => state.palette);
 
-  // Set tabMenu list 
+  // Set tabMenu list
   const tabList = [];
   let index = 1;
-  let left = 0; // + width, margin-left 
+  let left = 0; // + width, margin-left
   for (let key in palette) {
-    tabList.push({ id: index++, target: key, color: palette[key], left});
+    tabList.push({ id: index++, target: key, color: palette[key], left });
     left += 53;
   }
 
   // Set default Palette's colors
   const defaultColors = [
-    '#e03131', '#d6336c', '#fd7e14', '#fab005', '#37b24d', '#15aabf', '#228be6', '#7048e8',
+    '#e03131',
+    '#d6336c',
+    '#fd7e14',
+    '#fab005',
+    '#37b24d',
+    '#15aabf',
+    '#228be6',
+    '#7048e8',
   ];
   const humidColors = [
-    '#ffc9c9', '#fcc2d7', '#ffd8a8', '#ffec99', '#b2f2bb', '#99e9f2', '#a5d8ff', '#d0bfff',
+    '#ffc9c9',
+    '#fcc2d7',
+    '#ffd8a8',
+    '#ffec99',
+    '#b2f2bb',
+    '#99e9f2',
+    '#a5d8ff',
+    '#d0bfff',
   ];
 
-  // Selected layout & hexColor
+  // Selected layout & hex color
   const [target, setTarget] = useState('');
   const [hexColor, setHexColor] = useState('');
 
   // Hex color code
+  // Get random hex code
   const getRandomHexColor = () => {
     const letters = '0123456789ABCDEF';
     let hex = '#';
@@ -162,21 +190,23 @@ const ChangeTheme = () => {
     }
     return hex;
   };
-  const handleHexColor = e => setHexColor(e.target.value);
+
+  // Set hex code to Swatch
+  const onChange = e => setHexColor(e.target.value);
 
   // Handle color
   const handleLayoutColor = target => {
-    // Q. how to detect new hexCode after setHexColor????
-    const newHexColor = getRandomHexColor();
     setTarget(target);
-    setHexColor(newHexColor);
+    setHexColor(getRandomHexColor());
   };
 
   const changeLayoutColor = () => {
-    dispatch(setColor({
-      key: target,
-      value: hexColor
-    }));
+    dispatch(
+      setColor({
+        key: target,
+        value: hexColor,
+      }),
+    );
   };
 
   return (
@@ -184,45 +214,63 @@ const ChangeTheme = () => {
       <h3>테마 변경하기</h3>
       <ThemeWrapper>
         <TabList>
-          {
-            tabList.map(tab => (<Tab key={tab.id} color={tab.color} left={tab.left} onClick={() => handleLayoutColor(tab.target)}></Tab>))
-          }
+          {tabList.map(tab => (
+            <Tab
+              key={tab.id}
+              id={tab.target}
+              target={target}
+              color={tab.color}
+              left={tab.left}
+              onClick={() => handleLayoutColor(tab.target)}
+            ></Tab>
+          ))}
         </TabList>
         <ChangeWrapper color={palette[target]}>
-          {
-            target ? (<ColorWrapper>
+          {target ? (
+            <ColorWrapper>
               <Swatch color={hexColor}>
                 <input
                   type="color"
                   name={target}
                   value={hexColor}
-                  onChange={handleHexColor}
+                  onChange={onChange}
                 />
                 <p>{hexColor}</p>
                 <p>{target}</p>
               </Swatch>
               <PaletteWrapper>
-                <Arrow color={palette[target]} />
                 <ColorList>
-                {
-                  defaultColors.map((color, index) => (<Color key={index} color={color}></Color>))
-                }
+                  {defaultColors.map((color, index) => (
+                    <Color
+                      key={index}
+                      color={color}
+                      onClick={() => setHexColor(color)}
+                    ></Color>
+                  ))}
                 </ColorList>
                 <ColorList>
-                {
-                  humidColors.map((color, index) => (<Color key={index} color={color}></Color>))
-                }
+                  {humidColors.map((color, index) => (
+                    <Color
+                      key={index}
+                      color={color}
+                      onClick={() => setHexColor(color)}
+                    ></Color>
+                  ))}
                 </ColorList>
-                <Button type="button" onClick={() => setHexColor(getRandomHexColor())}>
+                <Button
+                  type="button"
+                  onClick={() => setHexColor(getRandomHexColor())}
+                >
                   랜덤!
                 </Button>
                 <Button type="button" onClick={changeLayoutColor}>
                   적용하기
                 </Button>
               </PaletteWrapper>
-            </ColorWrapper>)
-            : <div>선택하세요!</div>
-          }
+            </ColorWrapper>
+          ) : (
+            <div className="pleasePick">변경할 색상의 꼬리표를 선택하세요!</div>
+          )}
         </ChangeWrapper>
       </ThemeWrapper>
     </Post>
